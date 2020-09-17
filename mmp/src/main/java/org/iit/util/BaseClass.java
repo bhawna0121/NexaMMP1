f2ed94891cc1f4a2d0814061b9972f8fec456f8e
sudheer51 <sudheer694@gmail.com>
2020-09-17 07:23:12 +0530

    Adding Comments

--- mmp/src/main/java/org/iit/util/BaseClass.java
+++ mmp/src/main/java/org/iit/util/BaseClass.java
@@ -1,24 +1,15 @@
 package org.iit.util;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseClass {
-	
-	 
-		
-		 protected WebDriver driver ;
+	 	protected WebDriver driver;
 		@BeforeTest
 		public void instantiateDriver()
 		{
 			WebDriverManager.chromedriver().setup();
+			//Launching the browser
 			driver = new ChromeDriver();
-			
 		}
-		
-	 
-
 }
